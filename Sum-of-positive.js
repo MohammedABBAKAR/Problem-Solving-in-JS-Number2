6241b05b7e4007da3360e03f660a91b6c81dbe93
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2024-01-25 20:40:43 +0100

    Sum of positive Number2

---
+++ Sum-of-positive.js
@@ -0,0 +1,19 @@
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+
+
+function positiveSum(arr) {
+    let Sum =0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+        Sum += arr[i]
+    }
+  
+}
+return Sum
+}
+positiveSum([1,-4,7,12])
+console.log(positiveSum([1,-4,7,12]))
